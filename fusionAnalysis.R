0be357ac3973c022112ee7cc39795b9a94a5ce02
Sindiri <sindiris@nih.gov>
2019-02-19 10:06:25 -0500

    mc

--- fusionAnalysis.R
+++ fusionAnalysis.R
@@ -3,24 +3,24 @@ workDir = "/Users/sindiris/R Scribble/RNASeq.Fusion.data"
 setwd(paste0(workDir,"./AnnotatedFusionCalls"))
 
 ## REad well annotated DataFrame
-metaData <- read.csv(paste0(workDir,"/MetadataMapper.v2.txt"), header = T, sep = "\t", stringsAsFactors = FALSE); 
+metaData <- read.csv(paste0(workDir,"/MetadataMapper.v3.txt"), header = T, sep = "\t", stringsAsFactors = FALSE); 
 # metaData <- metaData %<>% dplyr::rename(SampleName = Biowulf_SampleName)
 head(metaData)
 
 cols <- c("Sample.ID", "case_id", "patient_id", "left_gene", "right_gene", "left_chr", "left_position", "right_chr", "right_position",
-          "sample_id", "tool", "spanreadcount", "type", "var_level","ACMG_v1","ACMG_v2" ,"Brain_Cancer","CGCensus_Hereditary","Colon_Cancer",
+          "sample_id", "tool", "spanreadcount", "type", "var_level","ACMG_v2" ,"Brain_Cancer","CGCensus_Hereditary","Colon_Cancer",
           "Fusion", "Fusion_Genes","Fusion_Genes_Illumina","Hotspot_Genes","Inherited_Diseases","Kidney_Cancer","Loss_Of_Function_Genes",
           "Melanoma_Germline","NIAID_Panel","Neuroblastoma_Germline","PAX3-FOXO1_Targets_ChipSeq","Tumor_Suppressor_Genes","clinomics_gene_list")
 
 ## Empty data matrix ####
 emptyDF <- data.frame(matrix(ncol = 32)); colnames(emptyDF) <- cols
 
-## function to concatenate files to a single matrix ####
+## function to concatenate files to a single matrix 
 makResultMatrix <- function(x) {
   #print(x)
   data <- tryCatch({
     
-    read.csv(paste0("./fusions.annotated/",x),sep="\t", header = FALSE, stringsAsFactors = FALSE )
+    read.csv(paste0("./fusions.annotated/",x),sep="\t", header = TRUE, stringsAsFactors = FALSE )
     
   }, error = function(e){
     
@@ -34,11 +34,11 @@ makResultMatrix <- function(x) {
   return(data)
 }
 
-### List all the files
-fusionFiles <- list.files("./fusions.annotated/")[-1]
+### List all the files ####
+fusionFiles <- list.files("./fusions.annotated/"); length(fusionFiles)
 
 ### concatenate files
-finalResultMatrix <- rbindlist(lapply(fusionFiles, makResultMatrix), fill = TRUE)[,-c(33)]
+finalResultMatrix <- rbindlist(lapply(fusionFiles, makResultMatrix), fill = TRUE)[,-c(32,33)]
 dim(finalResultMatrix); View(head(finalResultMatrix))
 
 ### Append metadata to the finalResultMatrix ###
