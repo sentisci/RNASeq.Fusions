8b15ac01acdf5e4f4b8f7a00595b95de6dc2355f
Sindiri <sindiris@nih.gov>
2019-03-28 13:46:52 -0400

    MC

--- fusionAnalysis.R
+++ fusionAnalysis.R
@@ -7,7 +7,7 @@ metaData <- read.csv(paste0(workDir,"/MetadataMapper.v3.txt"), header = T, sep =
 # metaData <- metaData %<>% dplyr::rename(SampleName = Biowulf_SampleName)
 head(metaData)
 
-cols <- c("Sample.ID", "case_id", "patient_id", "left_gene", "right_gene", "left_chr", "left_position", "right_chr", "right_position",
+cols <- c("case_id", "patient_id", "left_gene", "right_gene", "left_chr", "left_position", "right_chr", "right_position",
           "sample_id", "tool", "spanreadcount", "type", "var_level","ACMG_v2" ,"Brain_Cancer","CGCensus_Hereditary","Colon_Cancer",
           "Fusion", "Fusion_Genes","Fusion_Genes_Illumina","Hotspot_Genes","Inherited_Diseases","Kidney_Cancer","Loss_Of_Function_Genes",
           "Melanoma_Germline","NIAID_Panel","Neuroblastoma_Germline","PAX3-FOXO1_Targets_ChipSeq","Tumor_Suppressor_Genes","clinomics_gene_list")
@@ -27,19 +27,20 @@ makResultMatrix <- function(x) {
     emptyDF
   })
   #print(ncol(data))
-  fileName <- gsub(".annotated_fusions.txt", "",x ); samplenameList <- unlist(strsplit(fileName,"_")); 
-  samplename <- paste0(samplenameList[3:length(samplenameList)], collapse="_")
-  data <- cbind(Sample.Biowulf.ID =  samplename, data )
-  colnames(data) <- cols
+  # fileName <- gsub(".annotated_fusions.txt", "",x ); samplenameList <- unlist(strsplit(fileName,"_")); 
+  # samplename <- paste0(samplenameList[3:length(samplenameList)], collapse="_")
+  # data <- cbind(Sample.Biowulf.ID =  samplename, data )
+  # colnames(data) <- cols
   return(data)
 }
 
 ### List all the files ####
 fusionFiles <- list.files("./fusions.annotated/"); length(fusionFiles)
 
-### concatenate files
+### concatenate files and change "sample_id" to "Sample.ID"
 finalResultMatrix <- rbindlist(lapply(fusionFiles, makResultMatrix), fill = TRUE)[,-c(32,33)]
 dim(finalResultMatrix); View(head(finalResultMatrix))
+finalResultMatrix <- finalResultMatrix %>% dplyr::rename(Sample.ID=sample_id)
 
 ### Append metadata to the finalResultMatrix ###
 finalResultMatrixJoin <- dplyr::left_join(finalResultMatrix, metaData[,
@@ -52,6 +53,9 @@ df <- finalResultMatrixJoin %>% data.frame()
 df$Any_NA <- apply(df[,grep(paste( c("Patient.ID", "Sample.ID", "Sample.ID.Alias", "DIAGNOSIS.Alias", "LIBRARY_TYPE"),collapse = "|"), names(df) )],1, function(x) anyNA(x))
 df_NA_TRUE <- df[which(df$Any_NA == "TRUE"),] %>% dplyr::distinct(Sample.ID,DIAGNOSIS.Alias,LIBRARY_TYPE);dim(df_NA_TRUE); View(df_NA_TRUE)
 
+## Get only complecases 
+finalResultMatrixJoin <- finalResultMatrixJoin %>% filter(!Sample.ID %in% c(df_NA_TRUE$Sample.ID))
+dim(finalResultMatrixJoin) ; length(unique(finalResultMatrixJoin$Sample.ID))
 
 ## Make a key 
 finalResultMatrixJoin$key <- paste(finalResultMatrixJoin$left_chr, finalResultMatrixJoin$left_gene, finalResultMatrixJoin$right_chr, 
@@ -59,8 +63,8 @@ finalResultMatrixJoin$key <- paste(finalResultMatrixJoin$left_chr, finalResultMa
 ## Count samples with Defuse
 finalfusionResultMatrix.Defuse <- finalResultMatrixJoin %>% filter( grepl('Defuse', tool)) %>%
                                           dplyr::distinct(Patient.ID, Sample.ID, Case.ID)
-dim(finalfusionResultMatrix); View(finalfusionResultMatrix)
-#write.table(finalResultMatrixJoin, "../finalfusionResultMatrix.txt", sep="\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+dim(finalfusionResultMatrix.Defuse); View(finalfusionResultMatrix.Defuse)
+write.table(finalResultMatrixJoin, "../FinalfusionResultMatrix.txt", sep="\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
 ## Keep only max spanning read count.
 
 ##Read Annotation Data
@@ -74,7 +78,7 @@ leftGenePair    <- geneFusionLeft[which(!geneFusionLeft %in% Annotation$GeneName
 
 ## Remove disgusting samples . "NCIEWS5000muscle_T_D23N9ACXX" , "NCI0228normal_T_C3JV2ACXX" . I dont understand why tumor patient normal is used in
 ## RNASeq cohort experiment
-finalResultMatrixJoin.NoPatientNormal <-  finalResultMatrixJoin %>% filter(!Sample.ID %in% c("NCIEWS5000muscle_T_D23N9ACXX" ))
+finalResultMatrixJoin.NoPatientNormal <-  finalResultMatrixJoin %>% filter()
 dim(finalResultMatrixJoin.NoPatientNormal)
 
 ##Filter Fusion File
